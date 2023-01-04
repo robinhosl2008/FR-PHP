b2ec0abdfe96503c2d295009048547700a4cec06
Robson Souza Lourenço <robson.lourenco.pacheco@gmail.com>
2023-01-04 19:16:25 -0300

    Recriando framework com o php tips.

--- app/routers/routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    '/' => 'Home@index',
-    '/user' => 'User@index',
-    '/user/create' => 'User@create',
-    '/user/update/[0-9]+'=> 'User@update',
-    '/user/delete/[0-9]+'=> 'User@delete'
-];
